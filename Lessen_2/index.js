b7b9642502a24f19396c2028ab15993978d458cc
Boris-Maslenov <ya.maslenov@ya.ru>
2023-02-16 14:27:53 +0300

    static server folders +  html + images + 404

--- Lessen_2/index.js
+++ Lessen_2/index.js
@@ -1,47 +1,42 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
 const FOLDER_PATH = 'public';
-
 const PORT = process.env.PORT || 3000;
-const server = http.createServer((request, response) => {
 
-    //console.log(request.url);
-    //console.log(request.headers.accept.split('/')[0]); //
-    const headerAccept = request.headers.accept.split('/')[0];
-    console.log(headerAccept);
-    // если запрос на главную => ищем index.html, в противном случае ищем файл  по url в папке public
-    let  filePatch = path.join(__dirname, request.url === '/' ? `/${FOLDER_PATH}/index.html` : `/${FOLDER_PATH}/${request.url}`);
-    // Если в запросе нет расширения ищем index.html  в папке
-    path.extname(filePatch) === '' ? filePatch += 'index.html' : void 0;
-
-    function getHeaders(extName) {
-        const extNameNotDot = extName.replace('.', '');
-        switch( extNameNotDot ){
-        case 'jpg' :
-            return {'Content-Type' : 'image/jpg'};   
-        case 'JPG' :
-            return {'Content-Type' : 'image/jpg'};
-        case 'png' :
-                return {'Content-Type' : 'image/png'};
-        case 'ico' :
-                return {'Content-Type' : 'image/ico'};          
-        default :
-            return  {'Content-Type' : 'text/html'}; 
-        }
+function getHeaders(ext) {
+    switch(ext){
+    // if images:
+    case 'jpg' :
+    case 'JPG':
+    case 'png': 
+    case 'ico':
+    case 'svg':
+    case 'webp':
+    case 'gif':
+        return {'Content-Type' : `image/${ext}`};    
+    // add other ext-s....
+    default :
+        return  {'Content-Type' : 'text/html'}; 
     }
-    response.writeHead(200, getHeaders(path.extname(filePatch)));
+}
 
-    fs.readFile(filePatch, (err, data) => {
-        if(err){
-            response.end('<h1>404</h1>');
-        };
-        response.end(data);
-    });
-
+const server = http.createServer((request, response) => {
+    let  filePatch = path.join(__dirname, request.url === '/' ? `/${FOLDER_PATH}/index.html` : `/${FOLDER_PATH}/${request.url}`);
+    const extName = path.extname(filePatch); 
+    const extNameNotDot = extName.replace('.', '');
+    extName === '' ? filePatch += 'index.html' : void 0;
+        
+        fs.readFile(filePatch, (err, data) => {
+            if(err){
+                response.writeHead( 404, getHeaders(extNameNotDot) );
+                response.end('<h1>404 - Page Not Found</h1>'); // лучше возвращать страницу 404, но пока так
+            };
+            response.writeHead( 200, getHeaders(extNameNotDot) );
+            response.end(data);
+        });
 });
 
 server.listen(PORT ,() => {
     console.log(`Сервер успешно запущен на ${PORT} порту!`);
-})
+});
